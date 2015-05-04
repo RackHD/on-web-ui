b501a21181812ddb4ff098211b68c6737cc8c77d
Poulter, Roland <Roland.Poulter@emc.com>
2015-05-04 16:06:15 -0700

    adds crud api calls for nodes

--- src/actions/NodesActions.js
+++ src/actions/NodesActions.js
@@ -5,9 +5,9 @@ import http from 'superagent';
 
 export default {
 
-  getNode(id) {
+  getNodes() {
     return new Promise(function (resolve, reject) {
-      http.get(API + 'nodes/' + id)
+      http.get(API + 'nodes')
         .accept('json')
         .end((err, res) => {
           if (err) { return reject(err); }
@@ -16,23 +16,12 @@ export default {
     });
   },
 
-  patchNode(id, body) {
+  postNodes(body) {
     return new Promise(function (resolve, reject) {
-      http.patch(API + 'nodes/' + id)
+      http.post(API + 'nodes')
         .accept('json')
         .type('json')
         .send(body)
-        .end((err, res) => {
-          if (err) { return reject(err); }
-          resolve(res && res.body || res.text);
-        });
-    });
-  },
-
-  deleteNode(id) {
-    return new Promise(function (resolve, reject) {
-      http.delete(API + 'nodes/' + id)
-        .accept('json')
         .end((err, res) => {
           if (err) { return reject(err); }
           resolve(res && res.body || res.text);
