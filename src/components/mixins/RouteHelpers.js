8d8228c088af1ba163ad1c7a2949dcf430edeaec
Poulter, Roland <Roland.Poulter@emc.com>
2015-05-06 15:51:02 -0700

    es6 rest/spread args

--- src/components/mixins/RouteHelpers.js
+++ src/components/mixins/RouteHelpers.js
@@ -5,21 +5,12 @@ const delay = 100;
 
 export default {
 
-  routePath: function (path) {
-    if (arguments.length > 1) {
-      path = Array.prototype.slice.call(arguments, 0);
-    }
-    if (Array.isArray(path)) {
-      path = path.join('/');
-    }
-    return '#/' + path;
+  routePath: function (...path) {
+    return '#/' + path.join('/');
   },
 
-  routeTo: function () {
-    var args = arguments;
-    setTimeout(function () {
-      window.location = this.routePath.apply(this, args);
-    }.bind(this), delay);
+  routeTo: function (...path) {
+    setTimeout(() => window.location = this.routePath(...path), delay);
   },
 
   routeBack: () => setTimeout(window.history.back.bind(window.history), delay)
