71c9f3ccc5a3b40e34de237335d4a549c24ee162
Poulter, Roland <Roland.Poulter@emc.com>
2015-05-18 13:09:56 -0700

    adds dev feature flag

--- onrack/src/config.js
+++ onrack/src/config.js
@@ -1,4 +1,8 @@
 'use strict';
 
+import featureFlag from '../../common/lib/featureFlag';
+
 // export const API = 'http://onrackapi.hwimo.lab.emc.com/rest/v1/';
 export const API = 'http://localhost:2000/';
+
+export const devFlag = featureFlag('dev').on();
