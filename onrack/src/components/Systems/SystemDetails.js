51c6d63342834fc35b2063aa4a64119ec3f9b112
Poulter, Roland <Roland.Poulter@emc.com>
2015-05-29 15:33:53 -0700

    improves system details page

--- onrack/src/components/Systems/SystemDetails.js
+++ onrack/src/components/Systems/SystemDetails.js
@@ -9,6 +9,7 @@ import DeveloperHelpers from 'common-web-ui/mixins/DeveloperHelpers';
 /* eslint-enable no-unused-vars */
 
 import {} from 'material-ui';
+import JsonEditor from 'common-web-ui/components/JsonEditor';
 import { systems } from '../../actions/SystemActions';
 import { chassis } from '../../actions/ChassisActions';
 import ChassisDetails from '../Chassis/ChassisDetails';
@@ -31,7 +32,10 @@ export default class SystemDetails extends Component {
 
   componentWillUnmount() { this.unwatchSystem(); }
 
-  componentDidUpdate() { this.profileTime('SystemDetails', 'update'); }
+  componentDidUpdate() {
+    this.profileTime('SystemDetails', 'update');
+    this.refs.system.setState({value: this.state.system});
+  }
 
   render() {
     var system = this.state.system || {};
@@ -42,11 +46,57 @@ export default class SystemDetails extends Component {
           {href: 'systems', label: 'System'},
           this.getSystemId()
         )}
-        <h2>System</h2>
-        <h3>{system.name || 'Unknown'}</h3>
-        <div>{system.type || 'Unknown'}</div>
-        <div>{this.longDate(system.modified)}</div>
-        <div>{JSON.stringify(system)}</div>
+        <h3 className="right">{system.type || 'Unknown type.'}</h3>
+        <h2>{system.name || 'Unknown system.'}</h2>
+        <div className="clearfix">Modified: {this.longDate(system.modified)}</div>
+        <div className="container">
+          <div className="two columns">
+            <h4>BMC Information</h4>
+            <ul>
+              <li>Hostname: NodeName</li>
+              <li>IP Address: X.X.X.X</li>
+              <li>MAC Address: XXXXXXXXX</li>
+              <li>Firmware Version: X.XX</li>
+            </ul>
+            <h4>Server Actions</h4>
+            <ul>
+              <li>Power On Server</li>
+              <li>Shutdown Server</li>
+              <li>Power Off Server</li>
+              <li>Restart Server</li>
+              <li>Reset Server</li>
+              <li>Launch KVM Console</li>
+              <li>Map Virtual Media</li>
+              <li>Turn On Locator LED</li>
+            </ul>
+          </div>
+          <div className="eight columns">
+            <JsonEditor
+                initialValue={system}
+                updateParentState={this.updateSystem.bind(this)}
+                disabled={this.state.disabled}
+                ref="system" />
+          </div>
+          <div className="two columns">
+            <h4>Server Status</h4>
+            <ul>
+              <li>Power State: On</li>
+              <li>Server Status: Good</li>
+              <li>Processors: Good</li>
+              <li>Memory: Good</li>
+              <li>Power Supply: Good</li>
+              <li>Locator LED: Off</li>
+            </ul>
+            <h4>Server Properties</h4>
+            <ul>
+              <li>Node Model: Model Name</li>
+              <li>Serial Number: XXX-XXX-XXXX</li>
+              <li>UUID: XXXX-XXXX-XXXXXX</li>
+              <li>BIOS Version: XXX-XXX</li>
+              <li>Description: Lala Foofoo</li>
+            </ul>
+          </div>
+        </div>
         {system.chassis ? <ChassisDetails chassisId={system.chassis} /> : null}
       </div>
     );
@@ -57,5 +107,7 @@ export default class SystemDetails extends Component {
   readSystem() { return systems.read(this.getSystemId()); }
 
   readChassis(id) { return chassis.read(id); }
+
+  updateSystem(system) { this.setState({system: system}); }
 
 }
