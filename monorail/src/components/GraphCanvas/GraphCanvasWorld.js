6622ad9c6d5140f48c3fe3960889e8b867711fdb
Poulter, Roland <Roland.Poulter@emc.com>
2015-06-04 11:12:21 -0700

    adds gc world

--- monorail/src/components/GraphCanvas/GraphCanvasWorld.js
+++ monorail/src/components/GraphCanvas/GraphCanvasWorld.js
@@ -5,7 +5,6 @@ import React, { Component, PropTypes } from 'react';
 import mixin from 'react-mixin';
 import decorateComponent from 'common-web-ui/lib/decorateComponent';
 import StyleHelpers from 'common-web-ui/mixins/StyleHelpers';
-import DragEventHelpers from './mixins/DragEventHelpers';
 import WorldSpaceHelpers from './mixins/WorldSpaceHelpers';
 /* eslint-enable no-unused-vars */
 
@@ -16,39 +15,43 @@ import './GraphCanvasView.less';
 
 @decorateComponent({
   propTypes: {
+    initialViewX: PropTypes.number,
+    initialViewY: PropTypes.number,
+    initialScale: PropTypes.number,
     worldWidth: PropTypes.number,
-    worldHeight: PropTypes.number,
-    screenWidth: PropTypes.number,
-    screenHeight: PropTypes.number
+    worldHeight: PropTypes.number
   },
   defaultProps: {
+    initialViewX: 0,
+    initialViewY: 0,
+    initialSCale: 1,
     worldWidth: 800,
-    worldHeight: 600,
-    screenWidth: 400,
-    screenHeight: 300
+    worldHeight: 600
   }
 })
 @mixin.decorate(WorldSpaceHelpers)
-@mixin.decorate(DragEventHelpers)
 @mixin.decorate(StyleHelpers)
 export default class GraphCanvasView extends Component {
 
   state = {
-    screenPosition: new Vector(0, 0),
-    scale: 1,
+    viewPosition: new Vector(this.props.initialViewX, this.props.initialViewY),
+    scale: this.props.initialScale,
     marks: []
   };
 
+  updateViewPosition(viewPosition) {
+    this.setState({ viewPosition });
+  }
+
+  updateScale(scale) {
+    this.setState({ scale });
+  }
+
   render() { try {
-    var screenSize = this.screenSize,
-        worldSize = this.worldSize,
+    var worldSize = this.worldSize,
         gridBoundingBox = new Rectangle(0, 0, worldSize.x, worldSize.y),
         cssWorldSpaceTransform = {
           transform: this.worldSpaceTransform.toCSS3Transform()
-        },
-        cssScreenSize = {
-          width: screenSize.x,
-          height: screenSize.y
         },
         cssWorldSize = {
           width: worldSize.x,
@@ -56,55 +59,41 @@ export default class GraphCanvasView extends Component {
         };
     return (
       <div
-          className="GraphCanvasView"
-          onMouseDown={this.translateWorld()}
-          onWheel={this.scaleWorld.bind(this)}
-          style={cssScreenSize}>
+          ref="world"
+          className="world"
+          onDoubleClick={this.touchWorld.bind(this)}
+          style={this.mergeAndPrefix(cssWorldSpaceTransform, cssWorldSize)}>
+        <svg
+            className="vectors"
+            width={worldSize.x}
+            height={worldSize.y}
+            style={cssWorldSize}
+            viewBox={'0 0 ' + worldSize.x + ' ' + worldSize.y}
+            preserveAspectRatio="none"
+            xmlns="http://www.w3.org/2000/svg">
+          <GraphCanvasGrid
+              top={gridBoundingBox.top}
+              left={gridBoundingBox.left}
+              width={gridBoundingBox.width}
+              height={gridBoundingBox.height} />
+          {this.markVectors}
+        </svg>
         <div
-            ref="view"
-            className="view"
-            style={cssScreenSize}>
-          <div
-              ref="world"
-              className="world"
-              onDoubleClick={this.touchWorld.bind(this)}
-              style={this.mergeAndPrefix(cssWorldSpaceTransform, cssWorldSize)}>
-            <svg
-                className="vectors"
-                width={worldSize.x}
-                height={worldSize.y}
-                style={cssWorldSize}
-                viewBox={'0 0 ' + worldSize.x + ' ' + worldSize.y}
-                preserveAspectRatio="none"
-                xmlns="http://www.w3.org/2000/svg">
-              <GraphCanvasGrid
-                  top={gridBoundingBox.top}
-                  left={gridBoundingBox.left}
-                  width={gridBoundingBox.width}
-                  height={gridBoundingBox.height} />
-              {this.markVectors}
-            </svg>
-            <div
-              className="elements"
-              style={cssWorldSize}>
-              {this.markElements}
-            </div>
-          </div>
+          className="elements"
+          style={cssWorldSize}>
+          {this.markElements}
         </div>
       </div>
     );
   } catch (err) { console.error(err.stack || err); } }
 
-  translateWorld() {
-    return this.setupClickDrag(this.translateWorldListeners);
-  }
-
   touchWorld(event) {
     event.stopPropagation();
     event.preventDefault();
-    var domoff = this.domOffsetXY(event.currentTarget);
-    var client = new Vector(event.clientX, event.clientY);
-    var mark = client.sub(domoff).squish(this.scale);
+    // TODO: make it easier to convert event coords to world coords
+    var offset = this.domOffsetXY(event.currentTarget),
+        client = new Vector(event.clientX, event.clientY),
+        mark = client.sub(offset).squish(this.scale);
     console.log('Mark: ' + mark);
     this.setState(function(currentState) {
       return {marks: currentState.marks.concat([mark])};
