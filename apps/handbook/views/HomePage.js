c56bc72eb5481a6eebc12df6c3f06b8d18cbbcc9
Poulter, Roland <Roland.Poulter@emc.com>
2015-06-23 13:45:52 -0700

    wip: docs component

--- apps/handbook/views/HomePage.js
+++ apps/handbook/views/HomePage.js
@@ -12,12 +12,37 @@ import PageHelpers from 'common-web-ui/mixins/PageHelpers';
 import RouteHelpers from 'common-web-ui/mixins/RouteHelpers';
 
 import marked from 'marked';
-import docchi from 'docchi';
+import cod from 'cod';
+import highlight from 'highlight.js';
+import hljs from 'highlight.js/lib/languages/javascript';
+
+highlight.initHighlightingOnLoad();
+
+marked.setOptions({
+  langPrefix: 'hljs ',
+  highlight: function (code) {
+    return highlight.highlightAuto(code).value;
+  }
+});
 
 import http from 'superagent';
 let { codeServer } = window.config;
 
-// console.log(codeServer);
+function doc(cod) {
+  return [
+    <pre key={0} dangerouslySetInnerHTML={{__html: JSON.stringify(cod, null, 2)}} />,
+    <h1 key={1}>{cod.object.name} <sub>{cod.object.type}</sub> {cod.object.extends}</h1>,
+    <p key={2}>{cod.object.desc}</p>,
+    cod.method.map(function (method, index) {
+      return (
+        <div key={30 + index} className="method">
+          <h2>{method.name}</h2>
+          <p>{method.desc}</p>
+        </div>
+      );
+    })
+  ];
+}
 
 @radium
 @mixin.decorate(DeveloperHelpers)
@@ -34,6 +59,13 @@ let { codeServer } = window.config;
     style: null
   }
 })
+/**
+@object
+  @type class
+  @extends React.Component
+  @name HomePage
+  @desc Home page component
+*/
 export default class HomePage extends Component {
 
   state = {}
@@ -47,7 +79,15 @@ export default class HomePage extends Component {
       this.setState({readme: marked(body)});
     });
     this.getHandbookHomePage().then(body => {
-      this.setState({script: docchi.parse(body)});
+      this.setState({script: {
+        source: body,
+        code: marked('```javascript\n' + body + '\n```'),
+        docs: doc(cod(body, {
+          docsBegin: '/**',
+          docsEnd: '*/',
+          pretty: true
+        }))
+      }});
     });
   }
 
@@ -59,11 +99,19 @@ export default class HomePage extends Component {
         Welcome to the Home page.
         <div ref="apps">{this.state.apps}</div>
         <div ref="readme" dangerouslySetInnerHTML={{__html: this.state.readme}} />
-
+        {this.state.script ? [
+          <div ref="docs" key={0}>{this.state.script.docs}</div>,
+          <div ref="code" key={1} dangerouslySetInnerHTML={{__html: this.state.script.code}} />
+        ] : null}
       </div>
     );
   }
 
+  /**
+  @method
+    @name getApps
+    @desc Sends a HTTP request to get a list of app directories.
+  */
   getApps() {
     return new Promise(function (resolve, reject) {
       http.get(codeServer + '/')
@@ -74,6 +122,11 @@ export default class HomePage extends Component {
     });
   }
 
+  /**
+  @method
+    @name getReadMe
+    @desc Sends a HTTP request to get a README about apps.
+  */
   getReadMe() {
     return new Promise(function (resolve, reject) {
       http.get(codeServer + '/README.md')
@@ -85,6 +138,11 @@ export default class HomePage extends Component {
     });
   }
 
+  /**
+  @method
+    @name getHandbookHomePage
+    @desc Sends a HTTP request to get this source file in raw text.
+  */
   getHandbookHomePage() {
     return new Promise(function (resolve, reject) {
       http.get(codeServer + '/handbook/views/HomePage.js')
