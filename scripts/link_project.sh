748b04f4e3625421f58c9a56b1e154cbb8a966f3
Poulter, Roland <Roland.Poulter@emc.com>
2015-07-09 14:51:59 -0700

    switch to printf for newlines

--- scripts/link_project.sh
+++ scripts/link_project.sh
@@ -1,23 +1,28 @@
+printf '\n\nNPM link common-web-ui:'
 cd apps/common
 npm link
 cd ../../
 npm link common-web-ui
 
+printf '\n\nNPM link monorail-web-ui:'
 cd apps/monorail
 npm link
 cd ../../
 npm link monorail-web-ui
 
+printf '\n\nNPM link onrack-web-ui:'
 cd apps/onrack
 npm link
 cd ../../
 npm link onrack-web-ui
 
+printf '\n\nNPM link slush-app:'
 cd scripts
 npm link
 cd ../
 npm link slush-app
 
+printf '\n\nSetup material-ui module:'
 cd node_modules/material-ui
 npm install
 rm -rf node_modules/react
