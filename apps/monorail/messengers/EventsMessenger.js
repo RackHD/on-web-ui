25f2d926dbcd5294b863870d1d4ac3a74476da4e
Poulter, Roland <Roland.Poulter@emc.com>
2015-10-06 11:25:14 -0700

    adds events messenger

---
+++ apps/monorail/messengers/EventsMessenger.js
@@ -0,0 +1,39 @@
+// Copyright 2015, EMC, Inc.
+
+'use strict';
+
+import url from 'url';
+import { API } from '../config/index';
+
+import Messenger from 'common-web-ui/lib/Messenger';
+
+export default class WorkflowLogsMessenger extends Messenger {
+  handlers = ['item'];
+
+  constructor(host, secure) {
+    let api = url.parse(API);
+    super('mq', host || api.host, secure || api.protocol === 'https:');
+  }
+
+  item(msg) {
+    if (this.handler) this.handler(msg);
+  }
+
+  listen(handler) {
+    this.ignore();
+    setTimeout(() => {
+      this.handler = handler;
+      this.watch({
+        exchange: 'on.events',
+        routingKey: '#'
+      });
+    }, 100);
+  }
+
+  ignore() {
+    this.stop({
+      exchange: 'on.events',
+      routingKey: '#'
+    });
+  }
+}
