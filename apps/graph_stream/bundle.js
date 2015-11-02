ec448d73d85dd8200b7a96a13568f2fd763becdf
Poulter, Roland <Roland.Poulter@emc.com>
2015-11-02 12:54:13 -0800

    move graph_stream app

--- apps/graph_stream/bundle.js
+++
@@ -1,8 +0,0 @@
-// Copyright 2015, EMC, Inc.
-
-'use strict';
-
-// bootstrap bundle
-import './config/index';
-import './config/colors';
-import './config/routes';
--- apps/graph_stream/lib/Matrix.js
+++
@@ -1,5 +0,0 @@
-// Copyright 2015, EMC, Inc.
-
-'use strict';
-
-export default from 'graph-canvas-web-ui/lib/Matrix';
