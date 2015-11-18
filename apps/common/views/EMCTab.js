0780cc50492d8b5021c1354f02abe200d4b0d711
Poulter, Roland <Roland.Poulter@emc.com>
2015-11-17 18:48:05 -0800

    update emc tab

--- apps/common/views/EMCTab.js
+++ apps/common/views/EMCTab.js
@@ -2,26 +2,56 @@
 
 'use strict';
 
-import React, { // eslint-disable-line no-unused-vars
-  Component } from 'react';
+import React, { Component } from 'react';
 import radium from 'radium';
-import decorate from '../lib/decorate';
 
 @radium
-@decorate({
-  propTypes: {},
-  defaultProps: {}
-})
 export default class AppHeader extends Component {
+
+  // http://brand.emc.com/brand-elements-2/
+
+  css = {
+    root: {
+      backgroundColor: '#2C95DD',
+      overflow: 'hidden',
+      position: 'fixed',
+      display: 'block',
+      zIndex: 99,
+      color: 'white',
+      float: 'left',
+      // left: (100 / 21) + '%',
+      right: (100 / 21) + '%',
+      width: (100 / 7) + '%',
+      maxWidth: 140,
+      minWidth: 100,
+      bottom: 0,
+      // top: 0,
+
+      ':hover': {
+        backgroundColor: require('color')('#2C95DD').lighten(0.125).clearer(0.5).rgbaString()
+      }
+    },
+
+    logo: {
+      clear: 'both',
+      marginTop: '32%',
+      marginLeft: '16%',
+      marginRight: '16%',
+      marginBottom: '16%',
+      width: '64%'
+    }
+  }
 
   render() {
     return (
       <a
-          className="emc-tab right"
+          className="emc-tab"
+          style={[this.css.root]}
           href="http://emc.com">
 
         <img
             className="emc-logo"
+            style={[this.css.logo]}
             src="/common/WhiteLogoLarge.png"
             alt="EMC" />
 
