3e6413e46253fa230d6d163886d74b587a1ac308
Poulter, Roland <Roland.Poulter@emc.com>
2016-01-25 15:27:14 -0800

    Fix left nav deprecations

--- apps/common/views/AppNavigation.js
+++ apps/common/views/AppNavigation.js
@@ -15,59 +15,44 @@ import { LeftNav } from 'material-ui';
 export default class AppMenuNav extends Component {
 
   static propTypes = {
-    title: PropTypes.string,
-    menuItems: PropTypes.array,
-    brandStyle: PropTypes.object
   };
 
   static defaultProps = {
-    title: 'Dashboard',
-    menuItems: [],
-    brandStyle: {}
   };
-
-  toggleLeftNav = this.toggleLeftNav.bind(this);
-  getSelectedIndex = this.getSelectedIndex.bind(this);
-  onLeftNavChange = this.onLeftNavChange.bind(this);
 
   render() {
     return (
-      <LeftNav
-        className="AppMenuNav"
-        ref="leftNav"
-        docked={false}
-        isInitiallyOpen={false}
-        menuItems={this.props.menuItems}
-        selectedIndex={this.getSelectedIndex()}
-        onChange={this.onLeftNavChange} />
+      <LeftNav ref="leftNav"
+          className="AppMenuNav"
+          open={this.state.open}
+          docked={false}
+          onRequestChange={open => this.setState({open})}>
+        {this.props.children}
+      </LeftNav>
     );
   }
 
-  isActive(item) {
-    let end = window.location.hash.lastIndexOf('?'),
-        active = window.location.hash.substring(2, end).split('/')[0];
-    if (Array.isArray(item.route)) {
-      return item.route.indexOf(active) !== -1;
-    }
-    return item.route && item.route == active;
-  }
-
   toggleLeftNav() {
-    this.refs.leftNav.toggle();
+    this.setState({open: true});
   }
 
-  getSelectedIndex() {
-    var menuItems = this.props.menuItems,
-        currentItem;
-
-    for (var i = menuItems.length - 1; i >= 0; i--) {
-      currentItem = menuItems[i];
-      if (this.isActive(currentItem)) { return i; }
-    }
-  }
-
-  onLeftNavChange(e, key, payload) {
-    this.routeTo(Array.isArray(payload.route) ? payload.route[0] : payload.route);
-  }
+  // isActive(item) {
+  //   let end = window.location.hash.lastIndexOf('?'),
+  //       active = window.location.hash.substring(2, end).split('/')[0];
+  //   if (Array.isArray(item.route)) {
+  //     return item.route.indexOf(active) !== -1;
+  //   }
+  //   return item.route && item.route == active;
+  // }
+  //
+  // getSelectedIndex() {
+  //   var menuItems = this.props.menuItems,
+  //       currentItem;
+  //
+  //   for (var i = menuItems.length - 1; i >= 0; i--) {
+  //     currentItem = menuItems[i];
+  //     if (this.isActive(currentItem)) { return i; }
+  //   }
+  // }
 
 }
