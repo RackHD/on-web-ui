a0f3f453c4d192bf1bc483d108890aa4ea5c62c9
pengz1 <zuiliang.peng@emc.com>
2018-03-19 16:19:11 +0000

    Fix json editor import problem

--- src/app/in-memory-data.service.ts
+++
@@ -1,9 +0,0 @@
-import { InMemoryDbService } from 'angular-in-memory-web-api';
-export class InMemoryDataService implements InMemoryDbService {
-  createDb() {
-    const auth = [
-      { token: "12345", status: "200"}
-    ];
-    return {auth};
-  }
-}
--- src/app/json.ts
+++
@@ -1,4 +0,0 @@
-let Json = require('../../node_modules/jsoneditor/src/js/JSONEditor.js');
-// let test2 = require('../../node_modules/jsoneditor/src/css/jsoneditor.css');
-export JSONEditor = Json;
-// export JSONEditorCss = test2;
