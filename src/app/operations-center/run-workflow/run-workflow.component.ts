d4fc75ad5dbde13f628f89b66871a47dc3685f0e
chux1 <xiaoyu.chu@emc.com>
2018-04-19 16:10:51 +0800

    filter nodeId in run workflow

--- src/app/operations-center/run-workflow/run-workflow.component.ts
+++ src/app/operations-center/run-workflow/run-workflow.component.ts
@@ -9,6 +9,8 @@ import { JSONEditor } from '../../utils/json-editor';
 import { NodeService } from '../../services/rackhd/node.service';
 import { GraphService } from '../../services/rackhd/graph.service';
 import { WorkflowService } from '../../services/rackhd/workflow.service';
+import { CatalogsService } from '../../services/rackhd/catalogs.service';
+import { ObmService } from '../../services/rackhd/obm.service';
 
 @Component({
   selector: 'app-run-workflow',
@@ -32,21 +34,31 @@ export class RunWorkflowComponent implements OnInit {
   selectedFriendlyName: any;
   selectedInjectableName: any;
 
-  nodes: any;
-  selectedNodeId: any;
-  allNodeType: Array<any> = [];
+  allNodesInfo: Array<any> = [];
+  filteredNode: Array<any> = [];
+
+  allNodeTypes: Array<any> = [];
+  allNodeNames: Array<any> = [];
+  allNodeObms: Array<any> = [];
+  allNodeObmsName: Array<any> = [];
+  allNodeSkus: Array<any> = [];
+  allNodeSkusName: Array<any> = [];
+
   selectedNodeType: any;
   selectedNodeName: any;
   selectedNodeObm: any;
   selectedNodeSku: any;
 
+  selectedNodeId: any;
 
-  constructor(
-    public nodeService: NodeService,
-    public graphService: GraphService,
-    private activatedRoute: ActivatedRoute,
-    private workflowService: WorkflowService,
-    private router: Router) {}
+  constructor(public nodeService: NodeService,
+              public graphService: GraphService,
+              private activatedRoute: ActivatedRoute,
+              private workflowService: WorkflowService,
+              private catalogsService: CatalogsService,
+              private obmService: ObmService,
+              private router: Router) {
+  }
 
   ngOnInit() {
     this.showModal = false;
@@ -55,9 +67,6 @@ export class RunWorkflowComponent implements OnInit {
     this.editor = new JSONEditor(container, options);
     this.getAllNodes();
     this.getAllWorkflows();
-    NODE_TYPES.forEach(node => {
-      this.allNodeType.push(_.upperFirst(node));
-    });
     let searchTrigger = this.searchTerms.pipe(
       debounceTime(300),
       distinctUntilChanged(),
@@ -144,23 +153,182 @@ export class RunWorkflowComponent implements OnInit {
   /* some opertions about node*/
 
   getAllNodes() {
-    this.nodeService.getAll()
-      .subscribe(data => {
-        this.nodes = data;
-        console.log("all nodes ==== ",this.nodes);
-      });
+    if (this.filteredNode.length === 0) {
+      this.nodeService.getAll()
+        .subscribe(data => {
+          this.allNodesInfo = data;
+          console.log("allnodeinfo===", this.allNodesInfo);
+          this.getAllNames();
+          this.getAllSkus();
+          this.getAllObms();
+          this.getAllTypes();
+        });
+    } else {
+      this.allNodesInfo = this.filteredNode;
+      this.getAllNames();
+      this.getAllSkus();
+      this.getAllObms();
+      this.getAllTypes();
+    }
   }
 
-  getNodeTypes(type) {
-    console.log(_.lowerFirst(type));
-    this.nodeService.getByIdentifier(_.lowerFirst(type))
-      .subscribe(data => {
-        console.log(data);
+  /* Get the nodes with sku
+  *   1. there are some nodes without the property of sku. => filter them
+  *   2. if the sku is not null,then getting its name.
+  *   3. if the sku is null,
+  *      then judegging if his node type is compute.=>if not, fitler them
+  *   4. if the sku is null and its node type is compute,
+  *      get its dmi.base_board.product_name by catalogs.
+  **/
+  getAllSkus() {
+    let filterednodes;
+    if (this.filteredNode.length === 0)
+      filterednodes = this.allNodesInfo;
+    else
+      filterednodes = this.filteredNode;
+    this.allNodeSkus = [];
+    filterednodes.forEach(node => {
+      if (node.hasOwnProperty('sku')) {
+        if (node.sku !== null) {
+          let nodeTemp = {id: node.id, name: node.sku.name};
+          this.allNodeSkus.push(nodeTemp);
+        } else if (node.sku === null) {
+          if (node.type === "compute") {
+            this.catalogsService.getSource(node.id, "ohai").subscribe(data => {
+              let nodeTemp = {id: data.node, name: data.data.dmi.base_board.product_name};
+              this.allNodeSkus.push(nodeTemp);
+              this.allNodeSkusName = Array.from(new Set(_.map(this.allNodeSkus, 'name')));
+            });
+          }
+        }
+      }
+    });
+    console.log("all skus ==== ", this.allNodeSkus.length, this.allNodeSkus);
+    // console.log("all skus ==== ", this.allNodeSkusName.length, this.allNodeSkusName);
+  }
+
+  getAllNames() {
+    this.allNodeNames = [];
+    let filterednodes;
+    if (this.filteredNode.length === 0)
+      filterednodes = this.allNodesInfo;
+    else
+      filterednodes = this.filteredNode;
+    this.allNodeNames = _.map(filterednodes, 'name');
+    console.log("all names ==== ", this.allNodeNames.length, this.allNodeNames);
+  }
+
+  getAllObms() {
+    let filterednodes;
+    if (this.filteredNode.length === 0)
+      filterednodes = this.allNodesInfo;
+    else
+      filterednodes = this.filteredNode;
+    let obmInfo = [];
+    this.allNodeObms = [];
+    filterednodes.forEach(node => {
+      /* Get the nodes with obms*/
+      if (node.obms.length > 0) {
+        let obm = {id: node.id, obmId: node.obms[0].ref.split("/").pop()}
+        obmInfo.push(obm);
+      }
+    });
+    /* Get the host of obms*/
+    if (obmInfo.length > 0) {
+      obmInfo.forEach(value => {
+        this.obmService.getByIdentifier(value.obmId).subscribe(data => {
+          let obmTemp = {id: value.id, name: data.config.host};
+          this.allNodeObms.push(obmTemp);
+          this.allNodeObmsName = Array.from(new Set(_.map(this.allNodeObms, 'name')));
+        });
       });
+    }
+    // console.log("all obms ==== ", this.allNodeObmsName.length, this.allNodeObmsName);
+    console.log("all obms ==== ", this.allNodeObms.length, this.allNodeObms);
+  }
+
+  getAllTypes() {
+    this.allNodeTypes = [];
+    let filterednodes;
+    if (this.filteredNode.length === 0) {
+      NODE_TYPES.forEach(type => {
+        this.allNodeTypes.push(_.upperFirst(type));
+      });
+    } else {
+      this.filteredNode.forEach(node => {
+        this.allNodeTypes.push(_.upperFirst(node.type));
+      });
+    }
+    this.allNodeTypes = Array.from(new Set(this.allNodeTypes));
+    console.log("all types ==== ", this.allNodeTypes.length, this.allNodeTypes);
+  }
+
+  getNodeByIdentifier(type, value) {
+    console.log("filter===>", this.filteredNode);
+    let filterednodes;
+    if (this.filteredNode.length === 0)
+      filterednodes = this.allNodesInfo;
+    else
+      filterednodes = this.filteredNode;
+    switch (type) {
+      case 'nodetype':
+        this.filteredNode = _.filter(filterednodes, _.matches({'type': _.lowerFirst(value)}));
+        break;
+      case 'nodename':
+        this.filteredNode = _.filter(filterednodes, _.matches({'name': value}));
+        break;
+      case 'nodeobm':
+        let filterObmTemp = _.filter(this.allNodeObms, _.matches({'name': value}));
+        let obmTemp = [];
+        filterObmTemp.forEach(data => {
+          for (let node of this.filteredNode) {
+            if (node.id == data.id)
+              obmTemp.push(data)
+          }
+        })
+        this.filteredNode = obmTemp;
+        break;
+      case 'nodesku':
+        let filterSkuTemp = _.filter(this.allNodeSkus, _.matches({'name': value}));
+        console.log("第一次筛选sku：", filterSkuTemp);
+        let skuTemp = [];
+        filterSkuTemp.forEach(data => {
+          for (let node of this.filteredNode) {
+            if (node.id == data.id)
+              skuTemp.push(data)
+          }
+        })
+        this.filteredNode = skuTemp;
+        break;
+    }
+    // this.filteredNode = Array.from(new Set(this.filteredNode));
+    console.log("筛选之后的node信息", this.filteredNode.length, this.filteredNode);
+    if (this.filteredNode.length === 1) {
+      this.selectedNodeId = this.filteredNode[0].id;
+      this.selectedNodeType = _.upperFirst(this.filteredNode[0].type);
+      this.selectedNodeName = this.filteredNode[0].name;
+      if (this.filteredNode[0].obms.length > 0)
+        this.selectedNodeObm = _.filter(this.allNodeObms, _.matches({'id': this.filteredNode[0].id}))[0].name;
+      if (this.filteredNode[0].hasOwnProperty('sku')) {
+        this.selectedNodeSku = _.filter(this.allNodeSkus, _.matches({'id': this.filteredNode[0].id}))[0].name;
+      }
+    } else {
+      this.getAllNodes();
+    }
   }
 
   clearNodeInput() {
     this.selectedNodeId = null;
+    this.selectedNodeType = null;
+    this.selectedNodeName = null;
+    this.selectedNodeObm = null;
+    this.selectedNodeSku = null;
+    this.filteredNode = [];
+    this.getAllNodes();
+  }
+
+  clearOption(e) {
+    console.log(e.keyCode);
   }
 
   postWorflow() {
